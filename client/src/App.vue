0ee8b526b345291d149f7b930245fc0b73437a1e
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2021-02-25 16:05:46 +0100

    basic vue setup

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <i class="gargantuan material-icons">account_circle</i>
+    <HelloWorld msg="Welcome to the MediaDiary App" />
+    <Login />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Login from "./components/Login.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Login,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.gargantuan {
+  font-size: 250px;
+}
+</style>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row">
+    <div class="col push-m3 push-l3 s12 m6 l6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content row">
+          <span class="card-title white-text">Login</span>
+          <div class="input-field col s10 push-s1">
+            <input
+              placeholder="example@example.com"
+              id="email"
+              type="text"
+              class="validate"
+            />
+            <label for="email">email</label>
+          </div>
+          <div class="input-field col l5 s10 push-s1 push-l1">
+            <input
+              placeholder="password"
+              id="password"
+              type="password"
+              class="validate"
+            />
+            <label for="password">password</label>
+          </div>
+          <div class="col s12 push-s1 push-m1 m12 l6 push-l1">
+            <div class="row">
+              <a class="btn waves-effect waves-light col s4" name="login">
+                Log In
+                <i class="material-icons right">send</i>
+              </a>
+              <a class="waves-effect waves-light btn col s5 push-s1">
+                Register
+                <i class="material-icons right">person_add</i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import M from "materialize-css";
+import "materialize-css";
+import "materialize-css/dist/css/materialize.css";
+export default {
+  mounted() {
+    M.AutoInit();
+  },
+};
+</script>
+<style scoped>
+</style>
